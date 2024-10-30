5d9964aad4a5e47612328253f054e52d44729dfd
davfranco1 <david@francoonline.net>
2024-10-30 21:31:39 +0100

    Lab final

---
+++ src/soporte_funciones.py
@@ -0,0 +1,149 @@
+# Librerías para gestión de tiempos
+from time import sleep
+from tqdm import tqdm
+
+# Librerías para tratamiento de datos
+
+import pandas as pd
+import numpy as np
+
+# Librerías para captura de datos
+import requests
+
+# Librería para trabajar con bases de datos SQL
+import psycopg2
+from psycopg2 import OperationalError, errorcodes, errors
+
+# Librería para manejar archivos .env, para cargar tokens y claves
+import os
+import dotenv
+dotenv.load_dotenv()
+
+# Librería para ignorar avisos
+import warnings
+warnings.filterwarnings("ignore") # Ignora TODOS los avisos
+
+
+# Importamos el usuario y contraseña que hemos guardado en el archivo .env, de modo que podamos utilizarlos como inputs de nuestra función.
+dbeaver_pw = os.getenv("dbeaver_pw")
+dbeaver_user = os.getenv("dbeaver_user")
+
+
+def dbeaver_conexion(database):
+    """
+    Establece una conexión a una base de datos DBeaver.
+
+    Args:
+        database (str): El nombre de la base de datos.
+
+    Returns:
+        connection: Un objeto de conexión a la base de datos.
+    """
+    try:
+        conexion = psycopg2.connect(
+            database=database,
+            user=dbeaver_user,
+            password=dbeaver_pw,
+            host="localhost",
+            port="5432"
+        )
+    except OperationalError as e:
+        if e.pgcode == errorcodes.INVALID_PASSWORD:
+            print("Contraseña es errónea")
+        elif e.pgcode == errorcodes.CONNECTION_EXCEPTION:
+            print("Error de conexión")
+        else:
+            print(f"Ocurrió el error {e}")
+
+    return conexion
+
+
+def crear_db(database_name):
+    # conexion a postgres
+    conn = dbeaver_conexion(database_name) # Nos conectamos a la base de datos de postgres por defecto para poder crear la nueva base de datos
+    
+    # creamos un cursor con la conexion que acabamos de crear
+    cur = conn.cursor()
+    
+    cur.execute("SELECT 1 FROM pg_database WHERE datname = %s", (database_name,))
+    
+    # Almacenamos en una variable el resultado del fetchone. Si existe tendrá una fila sino será None
+    bbdd_existe = cur.fetchone()
+    
+    # Si bbdd_existe es None, creamos la base de datos
+    if not bbdd_existe:
+        cur.execute(f"CREATE DATABASE {database_name};")
+        print(f"Base de datos {database_name} creada con éxito")
+    else:
+        print(f"La base de datos ya existe")
+        
+    # Cerramos el cursor y la conexion
+    cur.close()
+    conn.close()
+
+
+
+def dbeaver_fetch(conexion, query):
+    """
+    Ejecuta una consulta y obtiene los resultados en un dataframe.
+
+    Args:
+        conexion (connection): Un objeto de conexión a la base de datos.
+        query (str): La consulta SQL a ejecutar.
+
+    Returns:
+        list: Los resultados de la consulta en un dataframe.
+    """
+    cursor = conexion.cursor()
+    cursor.execute(query)
+    # resultado_query = cursor.fetchall()
+    # Si quisiéramos que el resultado fuera en forma de lista podríamos utilizar esta línea de código.
+    # En este caso, sin embargo, nos interesa obtener directamente DFs.
+    
+    df = pd.DataFrame(cursor.fetchall())
+    df.columns = [col[0] for col in cursor.description]
+
+    cursor.close()
+    conexion.close()
+
+    return df
+
+
+def dbeaver_commit(conexion, query, *values):
+    """
+    Ejecuta una consulta y realiza un commit de los cambios.
+
+    Args:
+        conexion (connection): Un objeto de conexión a la base de datos.
+        query (str): La consulta SQL a ejecutar.
+        *values: Los valores a incluir en la consulta.
+
+    Returns:
+        str: Un mensaje de confirmación después del commit.
+    """
+    cursor = conexion.cursor()
+    cursor.execute(query, *values)
+    conexion.commit()
+    cursor.close()
+    conexion.close()
+    return print("Commit realizado")
+
+
+def dbeaver_commitmany(conexion, query, *values):
+    """
+    Ejecuta múltiples consultas y realiza un commit de los cambios.
+
+    Args:
+        conexion (connection): Un objeto de conexión a la base de datos.
+        query (str): La consulta SQL a ejecutar.
+        *values: Los valores a incluir en la consulta.
+
+    Returns:
+        str: Un mensaje de confirmación después del commit.
+    """
+    cursor = conexion.cursor()
+    cursor.executemany(query, *values)
+    conexion.commit()
+    cursor.close()
+    conexion.close()
+    return print("Commit realizado")
---
+++ src/soporte_variables.py
@@ -0,0 +1,148 @@
+query_creacion_edades = '''
+CREATE TABLE IF NOT EXISTS edades (
+    id_grupo_edad INT PRIMARY KEY,
+    grupo_edad VARCHAR
+);
+'''
+
+query_insercion_edades = '''
+    insert into edades (id_grupo_edad, grupo_edad)
+    values
+    (%s, %s)''' 
+
+
+query_creacion_ramas = '''
+CREATE TABLE IF NOT EXISTS ramas (
+    id_rama INT PRIMARY KEY,
+    nombre VARCHAR
+);
+'''
+
+query_insercion_ramas = '''
+    insert into ramas (id_rama, nombre)
+    values
+    (%s, %s)''' 
+
+
+query_creacion_comunidades = '''
+CREATE TABLE IF NOT EXISTS ccaa (
+    id_ccaa INT PRIMARY KEY,
+    nombre_ccaa VARCHAR
+);
+'''
+
+query_insercion_comunidades = '''
+    insert into ccaa (id_ccaa, nombre_ccaa)
+    values
+    (%s, %s)''' 
+
+
+query_creacion_provincias = '''
+CREATE TABLE IF NOT EXISTS provincias (
+    id_provincia INT PRIMARY KEY,
+    nombre_provincia VARCHAR,
+    id_ccaa INT REFERENCES ccaa(id_ccaa) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+'''
+
+query_insercion_provincias = '''
+    insert into provincias (id_provincia, nombre_provincia, id_ccaa)
+    values
+    (%s, %s, %s)''' 
+
+
+query_creacion_pib_total = '''
+CREATE TABLE IF NOT EXISTS pib_total (
+    id_pib_total SERIAL PRIMARY KEY,
+    id_provincia INT REFERENCES provincias(id_provincia) ON UPDATE CASCADE ON DELETE RESTRICT,
+    acumulado VARCHAR,
+    anio INT,
+    total FLOAT
+);
+'''
+
+query_insercion_pib_total = '''
+    insert into pib_total (id_provincia, acumulado, anio, total)
+    values
+    (%s, %s, %s, %s)''' 
+
+
+query_creacion_poblacion = '''
+CREATE TABLE IF NOT EXISTS poblacion (
+    id_poblacion SERIAL PRIMARY KEY,
+    id_provincia INT REFERENCES provincias(id_provincia) ON UPDATE CASCADE ON DELETE RESTRICT,
+    id_grupo_edad INT REFERENCES edades(id_grupo_edad) ON UPDATE CASCADE ON DELETE RESTRICT,
+    nacionalidad VARCHAR,
+    sexo VARCHAR,
+    anio INT,
+    total INT
+);
+'''
+
+query_insercion_poblacion = '''
+    insert into poblacion (id_provincia, id_grupo_edad, nacionalidad, sexo, anio, total)
+    values
+    (%s, %s, %s, %s, %s, %s)''' 
+
+
+query_creacion_pib = '''
+CREATE TABLE IF NOT EXISTS pib (
+    id_pib SERIAL PRIMARY KEY,
+    id_provincia INT REFERENCES provincias(id_provincia) ON UPDATE CASCADE ON DELETE RESTRICT,
+    id_rama INT REFERENCES ramas(id_rama) ON UPDATE CASCADE ON DELETE RESTRICT,
+    anio INT,
+    total FLOAT
+);
+'''
+
+query_insercion_pib = '''
+    insert into pib (id_provincia, id_rama, anio, total)
+    values
+    (%s, %s, %s, %s)''' 
+
+
+query_creacion_tipo_energia = '''
+CREATE TABLE IF NOT EXISTS tipo_energia (
+    id_tipo_energia INT PRIMARY KEY,
+    nombre VARCHAR
+);
+'''
+
+query_insercion_tipo_energia = '''
+    insert into tipo_energia (id_tipo_energia, nombre)
+    values
+    (%s, %s)''' 
+
+
+query_creacion_demanda = '''
+CREATE TABLE IF NOT EXISTS demanda (
+    id_demanda SERIAL PRIMARY KEY,
+    id_ccaa INT REFERENCES ccaa(id_ccaa) ON UPDATE CASCADE ON DELETE RESTRICT,
+    anio INT,
+    mes INT,
+    valor FLOAT
+);
+'''
+
+query_insercion_demanda = '''
+    insert into demanda (id_ccaa, anio, mes, valor)
+    values
+    (%s, %s, %s, %s)''' 
+
+
+query_creacion_generacion = '''
+CREATE TABLE IF NOT EXISTS generacion (
+    id_generacion SERIAL PRIMARY KEY,
+    id_tipo_energia INT REFERENCES tipo_energia(id_tipo_energia) ON UPDATE CASCADE ON DELETE RESTRICT,
+    id_ccaa INT REFERENCES ccaa(id_ccaa) ON UPDATE CASCADE ON DELETE RESTRICT,
+    mes INT,
+    anio INT,
+    valor FLOAT,
+    porcentaje FLOAT
+);
+'''
+
+query_insercion_generacion = '''
+    insert into generacion (id_tipo_energia, id_ccaa, mes, anio, valor, porcentaje)
+    values
+    (%s, %s, %s, %s, %s, %s)''' 
